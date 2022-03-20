3ad4a406eb6a993ddfff1dc3407e847b6531bb23
Sriharsha <98337856+sriharsha1629@users.noreply.github.com>
2022-03-20 16:40:27 +0530

    Create Script.js

---
+++ March/week 2/Script.js
@@ -0,0 +1,34 @@
+/*Assignment 1
+**Create 4 functions for addition,subtraction,multiplication,division
+*/
+
+function sum(num1,num2){
+	
+	let result = num1+num2;
+	return result
+}
+function sub(num1,num2){
+	
+	let result = num1-num2;
+	return result
+}
+function mul(num1,num2){
+	
+	let result = num1*num2;
+	return result
+}
+function div(num1,num2){
+	
+	let result = num1/num2;
+	return result
+}
+
+t = sum(7,8);
+console.log(t)
+u = sub(9,7);
+console.log(u)
+v = mul(15,12);
+console.log(v)
+w = div(12,02);
+console.log(w)
+
